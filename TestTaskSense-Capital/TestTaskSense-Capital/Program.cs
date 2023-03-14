26e280a3a770f9e7200a62508fe26189b7e0b4d5
SigachevAV <ton666013@gmail.com>
2023-03-15 00:44:19 +0300

    task done

---
+++ TestTaskSense-Capital/TestTaskSense-Capital/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestTaskSense_Capital
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new XOGame().SaveGame();
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
